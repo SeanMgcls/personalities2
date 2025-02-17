fbd7d3c3366333178ba6b9800020ce572d71a2f1
SeanMgcls <magcalassean@gmail.com>
2025-02-17 12:02:36 +0800

    3rd commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/personalities-2-materialui/",
+  base: "/personalities2-materialui/",
 })
